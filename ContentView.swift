d12cdd5ac5d5103ff453be4343462cc0ff4f2fcc
Spencer <sfung6124@stu.d214.org>
2025-04-10 10:44:05 -0500

    add text

--- ContentView.swift
+++ ContentView.swift
@@ -5,12 +5,7 @@ struct ContentView: View {
     @State var number2: Int = 0
     @State var answer: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        Text("\(answer)")
     }
 }
 
