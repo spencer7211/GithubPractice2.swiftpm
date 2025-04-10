a2519c0e632cc23724d4315ede53fe5ec07e465d
Devin Joseph <djoseph7181@stu.d214.org>
2025-04-10 10:41:28 -0500

    added number2

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1: Int = 0
+    @State var number2: Int = 0
     @State var answer: Int = 0
     var body: some View {
         VStack {
