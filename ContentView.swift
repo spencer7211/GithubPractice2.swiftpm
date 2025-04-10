476e3ee2beb1fe8f09a567e7987b5fbefb29bd95
Abhi Sorathiya <abhisorathiya408@gmail.com>
2025-04-10 10:33:59 -0500

    Added state var answer

--- ContentView.swift
+++ ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer: Int = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -10,3 +11,4 @@ struct ContentView: View {
         }
     }
 }
+
