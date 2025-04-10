19b259e26bab8f54b29386b868fc35c0a9a9f04c
Spencer <sfung6124@stu.d214.org>
2025-04-10 10:52:55 -0500

    add text for answer

--- ContentView.swift
+++ ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
     @State var number2: Int = 0
     @State var answer: Int = 0
     var body: some View {
+        Text("\(answer)")
         TextField("number1", value: $number1, format: .number)
         TextField("number2", value: $number2, format: .number)
     }
