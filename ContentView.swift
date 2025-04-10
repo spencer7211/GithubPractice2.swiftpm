8947275923da4706d2c74ae62cddc334bb22a535
Abhi Sorathiya <abhisorathiya408@gmail.com>
2025-04-10 10:45:29 -0500

    added Button that says add

--- ContentView.swift
+++ ContentView.swift
@@ -5,11 +5,8 @@ struct ContentView: View {
     @State var number2: Int = 0
     @State var answer: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Add") {
+            answer = number1 + number2
         }
     }
 }
